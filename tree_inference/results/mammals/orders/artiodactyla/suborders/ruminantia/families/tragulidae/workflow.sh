8fcb7a5ff0eccc51ea0c2ccaa59f93c311c7d414
dimbots <dimitriosbotskaris@gmail.com>
2016-06-08 15:10:32 +0200

    data

--- tree_inference/results/mammals/orders/artiodactyla/suborders/ruminantia/families/tragulidae/workflow.sh
+++ tree_inference/results/mammals/orders/artiodactyla/suborders/ruminantia/families/tragulidae/workflow.sh
@@ -6,22 +6,22 @@
 # from start to finish INGROUP and OUTGROUP have to be filled in
 
 
-INGROUP=Tylopoda
-OUTGROUP=Hippopotamidae
+INGROUP=Tragulina
+OUTGROUP=Giraffa
 
 # perform taxonomic name reconciliation on an input list of names.
 # creates a table of NCBI taxonomy identifiers (the taxa table).
 
-
-  if [ ! -e species.tsv ]; then
-    smrt taxize -r Tylopoda,Hippopotamidae -b
-fi
+ if [ ! -e species.tsv ]; then
+    smrt taxize -r Tragulina,Giraffa -b
+  fi 
 
 # align all phylota clusters for the species in the taxa table.
 # produces many aligned fasta files and a file listing these
 if [ ! -e aligned.txt ]; then
     smrt align
   fi
+
 # assign orthology among the aligned clusters by reciprocal BLAST
 export SUPERSMART_BACKBONE_MAX_DISTANCE="0.1"
  if [ ! -e merged.txt ]; then
@@ -35,18 +35,19 @@ export SUPERSMART_BACKBONE_MAX_COVERAGE="5"
  if [ ! -e supermatrix.phy ]; then
      smrt bbmerge
    fi
+
 # run an exabayes search on the supermatrix, resulting in a backbone
 # posterior sample
 export SUPERSMART_EXABAYES_NUMGENS="100000"
- if [ ! -e backbone.dnd ]; then
+if [ ! -e backbone.dnd ]; then
      smrt bbinfer --inferencetool=exabayes --cleanup
    fi
 
+
 # root the backbone sample  on the outgroup
- if [ ! -e backbone-rerooted.dnd ]; then
+if [ ! -e backbone-rerooted.dnd ]; then
     smrt bbreroot -g $OUTGROUP --smooth
   fi
-
 # calibrate the re-rooted backbone tree using treePL
 if [ ! -e chronogram.dnd ]; then
      smrt bbcalibrate --tree backbone-rerooted.dnd --supermatrix supermatrix.phy -f fossils.tsv
@@ -58,7 +59,7 @@ smrt consense -b 0.2 -i chronogram.dnd --prob
 
 # decompose the backbone tree into monophyletic clades. writes a directory
 # with suitable alignments for each clade
-export SUPERSMART_CLADE_MAX_DISTANCE="0.7"
+export SUPERSMART_CLADE_MAX_DISTANCE="0.5"
 export SUPERSMART_CLADE_MIN_DENSITY="0.2"
 export SUPERSMART_CLADE_MIN_COVERAGE="1"
 export SUPERSMART_CLADE_MAX_COVERAGE="10"
@@ -68,7 +69,7 @@ smrt bbdecompose -b
 smrt clademerge --enrich
 
 # run *BEAST for each clade
-smrt cladeinfer --ngens=20000000 --sfreq=1000 --lfreq=1000
+smrt cladeinfer --ngens=15000000 --sfreq=1000 --lfreq=1000
 
 # graft the *BEAST results on the backbone
 smrt cladegraft
