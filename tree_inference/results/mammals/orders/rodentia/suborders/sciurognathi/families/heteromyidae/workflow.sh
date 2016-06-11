a0d402ae1a3626f43101cd3b8657904522684c4a
dimbots <dimitriosbotskaris@gmail.com>
2016-06-11 11:09:15 +0200

    data

--- tree_inference/results/mammals/orders/rodentia/suborders/sciurognathi/families/heteromyidae/workflow.sh
+++ tree_inference/results/mammals/orders/rodentia/suborders/sciurognathi/families/heteromyidae/workflow.sh
@@ -23,7 +23,7 @@ smrt orthologize
 
 # merge the orthologous clusters into a supermatrix with exemplar
 # species, two per genus
-export SUPERSMART_BACKBONE_MIN_COVERAGE="2"
+export SUPERSMART_BACKBONE_MIN_COVERAGE="3"
 export SUPERSMART_BACKBONE_MAX_COVERAGE="5"
 smrt bbmerge
 
@@ -44,7 +44,7 @@ smrt consense -b 0.2 -i chronogram.dnd --prob
 
 # decompose the backbone tree into monophyletic clades. writes a directory
 # with suitable alignments for each clade
-export SUPERSMART_CLADE_MAX_DISTANCE="0.5"
+export SUPERSMART_CLADE_MAX_DISTANCE="0.3"
 export SUPERSMART_CLADE_MIN_DENSITY="0.2"
 export SUPERSMART_CLADE_MIN_COVERAGE="1"
 export SUPERSMART_CLADE_MAX_COVERAGE="10"
@@ -54,13 +54,13 @@ smrt bbdecompose -b
 smrt clademerge --enrich
 
 # run *BEAST for each clade
-smrt cladeinfer --ngens=30000000 --sfreq=1000 --lfreq=1000
+smrt cladeinfer --ngens=20000000 --sfreq=1000 --lfreq=1000
 
 # graft the *BEAST results on the backbone
 smrt cladegraft
 
-#prune negative branches
+#prune negative branches 
 
-smrt-utils prunetree -t final.nex -g "species_name" -f figtree
+smrt-utils prunetree -t final.nex -g "species_name"
 
-#output is final_pruned.nex 
+#output of the command is final_pruned.nex
