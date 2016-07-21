496790590213f3a3d29276383a86bb0eda567fac
dimbots <dimitriosbotskaris@gmail.com>
2016-07-21 12:08:18 +0200

    data

--- tree_inference/results/monocots/orders/zingiberales/workflow.sh
+++ tree_inference/results/monocots/orders/zingiberales/workflow.sh
@@ -6,12 +6,12 @@
 # from start to finish INGROUP and OUTGROUP have to be filled in
 
 
-INGROUP=Zingiberales
-OUTGROUP=Ananas
+INGROUP=Dioscoreales
+OUTGROUP=Croomia
 
 # perform taxonomic name reconciliation on an input list of names.
 # creates a table of NCBI taxonomy identifiers (the taxa table).
-smrt taxize -r Zingiberales,Ananas -b
+smrt taxize -r Dioscoreales,Croomia -b
 
 # align all phylota clusters for the species in the taxa table.
 # produces many aligned fasta files and a file listing these
@@ -23,7 +23,7 @@ smrt orthologize
 
 # merge the orthologous clusters into a supermatrix with exemplar
 # species, two per genus
-export SUPERSMART_BACKBONE_MIN_COVERAGE="3"
+export SUPERSMART_BACKBONE_MIN_COVERAGE="1"
 export SUPERSMART_BACKBONE_MAX_COVERAGE="5"
 smrt bbmerge
 
@@ -44,7 +44,7 @@ smrt consense -b 0.2 -i chronogram.dnd --prob
 
 # decompose the backbone tree into monophyletic clades. writes a directory
 # with suitable alignments for each clade
-export SUPERSMART_CLADE_MAX_DISTANCE="0.5"
+export SUPERSMART_CLADE_MAX_DISTANCE="0.2"
 export SUPERSMART_CLADE_MIN_DENSITY="0.2"
 export SUPERSMART_CLADE_MIN_COVERAGE="1"
 export SUPERSMART_CLADE_MAX_COVERAGE="10"
@@ -59,7 +59,3 @@ smrt cladeinfer --ngens=30000000 --sfreq=1000 --lfreq=1000
 # graft the *BEAST results on the backbone
 smrt cladegraft
 
-#prune negative branches
-smrt-utils prunetree -t final.nex -g "species_name" -f figtree
-
-#output of the command is final_pruned.nex
