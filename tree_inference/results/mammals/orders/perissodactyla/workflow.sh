01ee6d272cfb3a2b7c5b78b5324bd86131f79a93
dimbots <dimitriosbotskaris@gmail.com>
2016-08-31 09:20:52 +0200

    data

--- tree_inference/results/mammals/orders/perissodactyla/workflow.sh
+++ tree_inference/results/mammals/orders/perissodactyla/workflow.sh
@@ -6,12 +6,12 @@
 # from start to finish INGROUP and OUTGROUP have to be filled in
 
 
-INGROUP=
-OUTGROUP=Pan
+INGROUP=Perissodactyla
+OUTGROUP=Hippopotamidae
 
 # perform taxonomic name reconciliation on an input list of names.
 # creates a table of NCBI taxonomy identifiers (the taxa table).
-smrt taxize -r Perissodactyla,Pan -b
+smrt taxize -r Perissodactyla,Hippopotamidae -b
 
 # align all phylota clusters for the species in the taxa table.
 # produces many aligned fasta files and a file listing these
@@ -45,7 +45,7 @@ smrt consense -b 0.2 -i chronogram.dnd --prob
 # decompose the backbone tree into monophyletic clades. writes a directory
 # with suitable alignments for each clade
 export SUPERSMART_CLADE_MAX_DISTANCE="0.2"
-export SUPERSMART_CLADE_MIN_DENSITY="0.2"
+export SUPERSMART_CLADE_MIN_DENSITY="0.3"
 export SUPERSMART_CLADE_MIN_COVERAGE="1"
 export SUPERSMART_CLADE_MAX_COVERAGE="10"
 smrt bbdecompose -b
@@ -59,7 +59,7 @@ smrt cladeinfer --ngens=15000000 --sfreq=1000 --lfreq=1000
 # graft the *BEAST results on the backbone
 smrt cladegraft
 
-#prune negative branches 
+#prune negative branches
 smrt-utils prunetree -t final.nex -g "species_name" -f figtree
 
 #output of the command is final_pruned.nex
