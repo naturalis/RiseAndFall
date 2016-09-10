81583dfe0aafba4a43d9ada899c559f034aa49ba
dimbots <dimitriosbotskaris@gmail.com>
2016-09-10 14:02:26 +0200

    data

--- tree_inference/results/mammals/orders/artiodactyla/suborders/ruminantia/families/cervidae/workflow.sh
+++ tree_inference/results/mammals/orders/artiodactyla/suborders/ruminantia/families/cervidae/workflow.sh
@@ -6,24 +6,24 @@
 # from start to finish INGROUP and OUTGROUP have to be filled in
 
 
-INGROUP=Tragulina
+INGROUP=Cervidae
 OUTGROUP=Hippopotamidae
 
 # perform taxonomic name reconciliation on an input list of names.
 # creates a table of NCBI taxonomy identifiers (the taxa table).
-smrt taxize -r Tragulina,Hippopotamidae -b
+smrt taxize -r Cervidae,Hippopotamidae -b
 
 # align all phylota clusters for the species in the taxa table.
 # produces many aligned fasta files and a file listing these
 smrt align
 
 # assign orthology among the aligned clusters by reciprocal BLAST
-export SUPERSMART_BACKBONE_MAX_DISTANCE="0.2"
+export SUPERSMART_BACKBONE_MAX_DISTANCE="0.1"
 smrt orthologize
 
 # merge the orthologous clusters into a supermatrix with exemplar
 # species, two per genus
-export SUPERSMART_BACKBONE_MIN_COVERAGE="1"
+export SUPERSMART_BACKBONE_MIN_COVERAGE="2"
 export SUPERSMART_BACKBONE_MAX_COVERAGE="5"
 smrt bbmerge
 
@@ -42,19 +42,4 @@ smrt bbcalibrate --tree backbone-rerooted.dnd --supermatrix supermatrix.phy -f f
 smrt consense -b 0.2 -i chronogram.dnd --prob
 
 
-# decompose the backbone tree into monophyletic clades. writes a directory
-# with suitable alignments for each clade
-export SUPERSMART_CLADE_MAX_DISTANCE="0.5"
-export SUPERSMART_CLADE_MIN_DENSITY="0.2"
-export SUPERSMART_CLADE_MIN_COVERAGE="1"
-export SUPERSMART_CLADE_MAX_COVERAGE="10"
-smrt bbdecompose -b
 
-# merge all the alignments for each clades into a nexml file
-smrt clademerge --enrich
-
-# run *BEAST for each clade
-smrt cladeinfer --ngens=20000000 --sfreq=1000 --lfreq=1000
-
-# graft the *BEAST results on the backbone
-smrt cladegraft
