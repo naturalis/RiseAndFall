fcbe16f9d6812131efd0d032e36a49ed913ae490
dimbots <dimitriosbotskaris@gmail.com>
2016-09-23 11:13:55 +0200

    data

--- tree_inference/results/mammals/orders/artiodactyla/suborders/ruminantia/families/bovidae/bovidae/workflow.sh
+++ tree_inference/results/mammals/orders/artiodactyla/suborders/ruminantia/families/bovidae/bovidae/workflow.sh
@@ -6,65 +6,47 @@
 # from start to finish INGROUP and OUTGROUP have to be filled in
 
 
-INGROUP=Lagomorpha
-OUTGROUP=Cavia
+INGROUP=Bovidae
+OUTGROUP=Hippopotamidae
 
 # perform taxonomic name reconciliation on an input list of names.
 # creates a table of NCBI taxonomy identifiers (the taxa table).
+#smrt taxize -r Bovidae,Hippopotamidae -b
 
-if [ ! -e species.tsv ]; then
-    smrt taxize -r Lagomorpha,Cavia -b
-  fi 
 # align all phylota clusters for the species in the taxa table.
 # produces many aligned fasta files and a file listing these
-if [ ! -e aligned.txt ]; then
-    smrt align
-  fi
+#smrt align
 
 # assign orthology among the aligned clusters by reciprocal BLAST
-export SUPERSMART_BACKBONE_MAX_DISTANCE="0.1"
-if [ ! -e merged.txt ]; then
-    smrt orthologize
-  fi
+#export SUPERSMART_BACKBONE_MAX_DISTANCE="0.08"
+#smrt orthologize
 
 # merge the orthologous clusters into a supermatrix with exemplar
 # species, two per genus
-export SUPERSMART_BACKBONE_MIN_COVERAGE="2"
-export SUPERSMART_BACKBONE_MAX_COVERAGE="5"
-if [ ! -e supermatrix.phy ]; then
-     smrt bbmerge
-   fi
-
+#export SUPERSMART_BACKBONE_MIN_COVERAGE="2"
+#export SUPERSMART_BACKBONE_MAX_COVERAGE="5"
+#smrt bbmerge
 
 # run an exabayes search on the supermatrix, resulting in a backbone
 # posterior sample
-export SUPERSMART_EXABAYES_NUMGENS="100000"
- if [ ! -e backbone.dnd ]; then
-     smrt bbinfer --inferencetool=exabayes --cleanup
-   fi
-
+#export SUPERSMART_EXABAYES_NUMGENS="100000"
+#smrt bbinfer --inferencetool=exabayes --cleanup
 
 # root the backbone sample  on the outgroup
- if [ ! -e backbone-rerooted.dnd ]; then
-    smrt bbreroot -g $OUTGROUP --smooth
-  fi
+#smrt bbreroot -g $OUTGROUP --smooth
 
 # calibrate the re-rooted backbone tree using treePL
+#smrt bbcalibrate --tree backbone-rerooted.dnd --supermatrix supermatrix.phy -f fossils.tsv
 
-if [ ! -e chronogram.dnd ]; then
-     smrt bbcalibrate --tree backbone-rerooted.dnd --supermatrix supermatrix.phy -f fossils.tsv
-   fi
 # build a consensus
-if [ ! -e consensus.nex ]; then
-      smrt consense -b 0.2 -i chronogram.dnd --prob 
-   fi
+#smrt consense -b 0.2 --prob -i chronogram.dnd --prob
 
 
 # decompose the backbone tree into monophyletic clades. writes a directory
 # with suitable alignments for each clade
-export SUPERSMART_CLADE_MAX_DISTANCE="0.3"
-export SUPERSMART_CLADE_MIN_DENSITY="0.2"
-export SUPERSMART_CLADE_MIN_COVERAGE="1"
+export SUPERSMART_CLADE_MAX_DISTANCE="0.2"
+export SUPERSMART_CLADE_MIN_DENSITY="0.3"
+export SUPERSMART_CLADE_MIN_COVERAGE="3"
 export SUPERSMART_CLADE_MAX_COVERAGE="10"
 smrt bbdecompose -b
 
@@ -76,9 +58,3 @@ smrt cladeinfer --ngens=30000000 --sfreq=1000 --lfreq=1000
 
 # graft the *BEAST results on the backbone
 smrt cladegraft
-
-#pruning negative branches
-
-smrt-utils prunetree -t final.nex -g "species_name"
-
-#output final_pruned.nex
