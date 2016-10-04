e9fab32abe3afbc088a0859ecef014c8f783c596
dimbots <dimitriosbotskaris@gmail.com>
2016-10-04 17:31:34 +0200

    data

--- tree_inference/results/mammals/orders/artiodactyla/suborders/ruminantia/families/cervidae/workflow.sh
+++ tree_inference/results/mammals/orders/artiodactyla/suborders/ruminantia/families/cervidae/workflow.sh
@@ -42,4 +42,19 @@ smrt bbcalibrate --tree backbone-rerooted.dnd --supermatrix supermatrix.phy -f f
 smrt consense -b 0.2 -i chronogram.dnd --prob
 
 
+# decompose the backbone tree into monophyletic clades. writes a directory
+# with suitable alignments for each clade
+export SUPERSMART_CLADE_MAX_DISTANCE="0.3"
+export SUPERSMART_CLADE_MIN_DENSITY="0.2"
+export SUPERSMART_CLADE_MIN_COVERAGE="3"
+export SUPERSMART_CLADE_MAX_COVERAGE="10"
+smrt bbdecompose -b
 
+# merge all the alignments for each clades into a nexml file
+smrt clademerge --enrich
+
+# run *BEAST for each clade
+smrt cladeinfer --ngens=20000000 --sfreq=1000 --lfreq=1000
+
+# graft the *BEAST results on the backbone
+smrt cladegraft
